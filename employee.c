a81f29e5f49c111fffea0d3ed8b33bfe912bf123
Priyadharshini7136 <priyadharshinis23aid@vetias.ac.in>
2025-05-13 13:04:58 +0530

    Add files via upload

---
+++ employee.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int age;
+    float salary;
+    char name[30];
+    printf("enter age:");
+    scanf("%d",&age);
+    printf("enter salary:");
+    scanf("%f\n",&salary);
+    printf("enter a name:");
+    scanf("%s",name);
+    return 0;
+}
